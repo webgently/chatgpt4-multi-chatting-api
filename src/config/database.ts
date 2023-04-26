f28d1afd731e64862c87b25278e5877c8a6d769d
webgently <oleksandrshkabura811@gmail.com>
2023-04-26 03:02:04 +0300

    feat: default admin account

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,7 @@
 require('dotenv').config();
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import { UserSchema } from '../models';
 
 const ConnectDatabase = async (mongoUrl: string) => {
   try {
@@ -13,10 +15,37 @@ const ConnectDatabase = async (mongoUrl: string) => {
     const result: any = await mongoose.connect(mongoUrl, connectOptions);
     if (result) {
       console.log('MongoDB connected');
+      await MakeAdminAccount();
     }
   } catch (err: any) {
     console.log(err);
     await ConnectDatabase(mongoUrl);
+  }
+};
+
+const MakeAdminAccount = async () => {
+  try {
+    const admin = {
+      first_name: process.env.ADMIN_NAME,
+      last_name: '',
+      email: process.env.ADMIN_EMAIL,
+      user_name: process.env.ADMIN_USER_NAME,
+      password: await bcrypt.hash(String(process.env.ADMIN_PASSWORD), 10),
+      permission: 'admin',
+      status: 'accept'
+    };
+
+    const result = await UserSchema.findOne({ permission: 'admin' });
+    if (!result) {
+      const newData = new UserSchema(admin);
+      const saveData = await newData.save();
+      if (!saveData) {
+        throw new Error('Database Error');
+      }
+      return;
+    }
+  } catch (err: any) {
+    console.log(err);
   }
 };
 
