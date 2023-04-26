ed5b68cddb7cb2908f4c339c1f83810723173609
webgently <oleksandrshkabura811@gmail.com>
2023-04-26 03:08:44 +0300

    update: serve file structure

--- src/serve.ts
+++ src/serve.ts
@@ -1,9 +1,14 @@
+require('dotenv').config();
 import express, { Express } from 'express';
 import cors from 'cors';
 
 // External Modules
+import API from './apis';
 import config from './config';
 import ConnectDatabase from './config/database';
+
+// Get router
+const router = express.Router();
 
 const app: Express = express();
 const port: Number = Number(process.env.HTTP_PORT || 2083);
@@ -17,6 +22,20 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Frontend Load
+app.use(express.static(__dirname + '/build'));
+app.get('/*', function (req: any, res: any) {
+  res.sendFile(__dirname + '/build/index.html', function (err: any) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+// API Router
+API(router);
+app.use('/api', router);
 
 ConnectDatabase(config.mongoURI);
 app.listen(port, () => {
