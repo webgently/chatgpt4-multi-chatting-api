9d3f4e60fceec055976276ce2eaf1191b303d46f
webgently <oleksandrshkabura811@gmail.com>
2023-04-27 23:37:53 +0300

    feat: add register and login type

--- src/interfaces/global.ts
+++ src/interfaces/global.ts
@@ -1,1 +1,13 @@
-export {};
+export interface RegisterObject {
+  first_name: string;
+  last_name: string;
+  email: string;
+  user_name: string;
+  password: string;
+  permission: string;
+}
+
+export interface LoginObject {
+  email: string;
+  password: string;
+}
