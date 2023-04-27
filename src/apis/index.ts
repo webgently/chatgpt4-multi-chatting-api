2aa6e5773e828e1e1a6c1081f24b2a86ee39a3fd
webgently <oleksandrshkabura811@gmail.com>
2023-04-28 00:46:38 +0300

    feat: declare login router

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,6 +3,7 @@ import AUTH from './auth';
 const API = (router: any) => {
   // APIs for Auth
   router.post('/register', AUTH.register);
+  router.post("/login", AUTH.login);
 };
 
 export default API;
