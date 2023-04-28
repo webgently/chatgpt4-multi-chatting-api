1c570eac19698b332684cceac288a2ea29d787f1
webgently <oleksandrshkabura811@gmail.com>
2023-04-29 01:38:01 +0300

    feat: implement socket structure

--- src/serve.ts
+++ src/serve.ts
@@ -1,27 +1,28 @@
 require('dotenv').config();
+import http from 'http';
 import express, { Express } from 'express';
 import cors from 'cors';
+import { Server } from 'socket.io';
 
 // External Modules
 import API from './apis';
 import config from './config';
 import ConnectDatabase from './config/database';
+import { initSocket } from './socket';
 
 // Get router
 const router = express.Router();
 
 const app: Express = express();
-const port: Number = Number(process.env.HTTP_PORT || 2083);
+const port: Number = Number(process.env.HTTP_PORT || 5000);
+const server = http.createServer(app);
 
-app.use(
-  cors({
-    origin: '*',
-    methods: ['POST', 'GET']
-  })
-);
-
+app.use(cors({ origin: '*', methods: ['POST', 'GET'] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+
 
 // Frontend Load
 app.use(express.static(__dirname + '/build'));
@@ -37,7 +38,12 @@ app.get('/*', function (req: any, res: any) {
 API(router);
 app.use('/api', router);
 
+// Socket Connect
+const io = new Server(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
+initSocket(io);
+app.set('io', io);
+
 ConnectDatabase(config.mongoURI);
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
