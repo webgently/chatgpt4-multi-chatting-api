5cff47af471f3b2af6a65151a122a3eaf1071483
webgently <oleksandrshkabura811@gmail.com>
2023-05-25 08:47:05 +0300

    feat: add admin controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
 import Auth from './auth';
 import Token from './token';
+import Admin from './admin';
 import ChatHistory from './chathistories';
 
-export = { Auth, Token, ChatHistory };
+export = { Auth, Token, ChatHistory, Admin };
