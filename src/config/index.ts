f4d2aa70210b4787bdaa35551c6b6b9abdd3ca8d
webgently <oleksandrshkabura811@gmail.com>
2023-05-25 08:47:20 +0300

    update: config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
 export = {
-  mongoURI: `mongodb://localhost:27017/${process.env.DATABASE_NAME}?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000`
+  mongoURI: `mongodb://127.0.0.1:27017/${process.env.DATABASE_NAME}?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000`
 };
