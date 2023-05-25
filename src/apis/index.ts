20a668a763a2749f33e248f175be87520635ade9
webgently <oleksandrshkabura811@gmail.com>
2023-05-25 08:47:43 +0300

    update: add any router

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,14 @@
 import AUTH from './auth';
+import History from './history';
+import Admin from './admin';
 
 const API = (router: any) => {
   // APIs for Auth
   router.post('/register', AUTH.register);
   router.post('/login', AUTH.login);
+  // APIs for Admin
+  router.post('/getAllUsers', Admin.getUsers);
+  router.post('/clearHistory', History.removeHistory);
   router.post('/updateStatus', AUTH.updateStatus);
 };
 
